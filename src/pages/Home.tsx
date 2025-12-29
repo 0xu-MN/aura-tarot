770e14a300d32c71db9d6348f82bb91e761f93f4
0xu-MN <hong56800@gmail.com>
2025-12-29 14:14:10 +0900

    style: Reduce Weekly Fortune thumbnail zoom to 110 percent

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,7 +57,7 @@ const Home = () => {
                                 <img
                                     src={weeklyThumb}
                                     alt="Weekly Horoscope"
-                                    className="w-full h-full object-cover opacity-80 scale-125 group-hover:scale-[1.35] transition-transform duration-700"
+                                    className="w-full h-full object-cover opacity-80 scale-110 group-hover:scale-125 transition-transform duration-700"
                                 />
                                 <div className="absolute inset-0 bg-gradient-to-r from-black/60 to-transparent" />
                             </div>
